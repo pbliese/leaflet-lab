066c7db75a392395b9492c9403796c90eccced19
pbliese <42621549+pbliese@users.noreply.github.com>
2019-02-25 20:10:13 -0600

    Module 5A assignment completed
    Updated main.js to have code for proportional symbols based on geojson data, added comments to code to further explain it.

--- js/main.js
+++ js/main.js
@@ -2,20 +2,28 @@
 
 //function to instantiate the Leaflet map
 function createMap(){
-    //create the map
+    //create the map////Set map so that whole world can be viewed at the start
     var map = L.map('mapdiv', {
         center: [20, 0],
         zoom: 2
     });
 
-    //add OSM base tilelayer
+    //add OSM base tilelayer////OSM to ease use, avoid having to go through keys
     L.tileLayer('http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
         attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap contributors</a>'
     }).addTo(map);
 
-    //call getData function
+    //call getData function////Main function of code, processes data and adds it to map
     getData(map);
 };
+////Create function to calculate the radius of proportional symbols to be called later
+function calcCircleRadius(attValue) {
+    var scaleFactor = .01;////Set scale factor to multiply data by. Here it's small because my data is in millions of USD, so most table values have five digits, some six
+    var area = (attValue*scaleFactor);
+    var radius = Math.sqrt(area/Math.PI);//use Math to calculate the radius based on scale and attribute value, then return value so it can be used later
+    return radius;
+};
+
 //function to retrieve the data and place it on the map
 function getData(map){
     //load the data////use ajax to simplify, load the data efficiently
@@ -23,6 +31,7 @@ function getData(map){
         dataType: "json",
         success: function(response){
             //create marker options////Circle markers, define the color and size of the marks. Use this section of code to control the marks
+            var attribute = "Amt_2017"////Define whice column of data we are accessing for symbols
             var geojsonMarkerOptions = {
                 radius: 8,
                 fillColor: "#ff7800",
@@ -35,6 +44,8 @@ function getData(map){
             //create a Leaflet GeoJSON layer and add it to the map////Use response from ajax to loop for each mega city
             L.geoJson(response, {
                 pointToLayer: function (feature, latlng){
+                    var attValue = Number(feature.properties[attribute]);////Get string from feature, then convert it to Number
+                    geojsonMarkerOptions.radius = calcCircleRadius(attValue);////Alter the radius by calling the function from above, supply desired attribute as keyword
                     return L.circleMarker(latlng, geojsonMarkerOptions);
                 }
             }).addTo(map);
